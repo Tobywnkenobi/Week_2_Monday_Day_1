43a9b1d2428b433c018bbad71d165d7bd7416fd0
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-08-28 14:01:05 -0400

    lunch break

--- Day-1/wb.py
+++ Day-1/wb.py
@@ -7,3 +7,41 @@
 # is_palindrome("mom")  \\ => True
 # is_palindrome("Shoha") \\ => False
 
+
+"""
+
+input string of letters
+output=boolean
+contraints: inputs only letters, only one word
+Racecar
+
+approach:
+look at each letter and check equality
+if unequal then non palindrome
+if equal look at next letters
+next letters moving first letter one to right
+last letter one to left
+continue checking all letters for equality until unequal or i've checked to the middle
+
+sudo code:
+if not letter=
+return false
+else=
+continue
+"""
+
+
+def check_palindrome(astring):
+    left_point = 0
+    right_point = len(astring) - 1
+    while left_point < right_point:
+        if astring[left_point].lower() != astring[right_point].lower():
+            return False
+        else:
+            left_point += 1
+            right_point -= 1
+    return True
+    
+print(check_palindrome("RaceCar"))
+print(check_palindrome("Mom"))
+print(check_palindrome("Shoha"))
